8a273c9a8b504eb67ec71fd53792fa0f5d4df83a
Akash <akashkumar_y@trisysit.com>
2021-01-04 15:18:27 +0530

    get location of user

--- Todo/AddTodo.js
+++ Todo/AddTodo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { addTodo } from '../action'
+import Geolocation from '@react-native-community/geolocation';
 
 class AddTodo extends Component {
     constructor(props){
@@ -25,6 +26,21 @@ class AddTodo extends Component {
             text:''
         })
     } 
+    alertLocation = () =>{
+        Geolocation.getCurrentPosition(
+            //Will give you the current location
+            (position) => {
+                alert(position.coords.latitude, position.coords.longitude, "location")
+                // this.getGeoCoding(position.coords.latitude, position.coords.longitude);
+            },
+            (error) => {
+                alert("message",error.message)
+                console.log(error.message);
+            },
+            { enableHighAccuracy: false, timeout: 20000 })
+
+    }
+
       
     render() {
         return (
@@ -38,6 +54,9 @@ class AddTodo extends Component {
              />
              <TouchableOpacity onPress={this.submitText} style={{width:70,height:30}}>
                  <Text>Submit</Text>
+             </TouchableOpacity>
+             <TouchableOpacity onPress={this.alertLocation} style={{width:70,height:30}}>
+                 <Text>LocationTracer</Text>
              </TouchableOpacity>
 
             </View>
